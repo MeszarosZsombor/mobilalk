fbd32c3d7d7e2016c60df3d3fc5148c7227a9de3
Mészáros Zsombor <h267827@stud.u-szeged.hu>
2024-04-02 17:56:28 +0200

    login and register

--- app/src/main/java/com/example/mobilalk/RegisterActivity.java
+++ app/src/main/java/com/example/mobilalk/RegisterActivity.java
@@ -1,9 +1,7 @@
 package com.example.mobilalk;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -13,20 +11,22 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class RegisterActivity extends AppCompatActivity {
 
-    private static final String LOG_TAG = MainActivity.class.getName();
+    private static final String LOG_TAG = RegisterActivity.class.getName();
     private static final String PREF_KEY = MainActivity.class.getPackage().toString();
-    private static final int SECRET_KEY = 99;
 
     EditText usernameET;
+    EditText emailET;
+    EditText phoneET;
     EditText passwordET;
-    private SharedPreferences preferences;
+    EditText rePasswordET;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -34,30 +34,18 @@ public class MainActivity extends AppCompatActivity {
         });
 
         usernameET = findViewById(R.id.editTextUsername);
+        emailET = findViewById(R.id.editTextEmail);
+        phoneET = findViewById(R.id.editTextPhone);
         passwordET = findViewById(R.id.editTextPassword);
 
-        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
-    }
+        SharedPreferences preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        String username = preferences.getString("username", "");
+        String password = preferences.getString("password", "");
 
-    public void login(View view) {
-        String username = usernameET.getText().toString();
-        String password = passwordET.getText().toString();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("username", usernameET.getText().toString());
-        editor.putString("password", passwordET.getText().toString());
-        editor.apply();
+        usernameET.setText(username);
+        passwordET.setText(password);
     }
 
     public void register(View view) {
-        Log.i(LOG_TAG, "registerOpen");
-        Intent intent = new Intent(this, RegisterActivity.class);
-        Log.i(LOG_TAG, intent.toString());
-        startActivity(intent);
     }
 }
